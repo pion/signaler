0386c2406990099862a34d2f8982b9adf9551942
Sean DuBois <sean@pion.ly>
2018-02-23 01:17:51 -0800

    signaler,client: Finish MVP one room with no input validation

--- api/websocket.go
+++ api/websocket.go
@@ -1,13 +1,86 @@
 package api
 
 import (
+	"encoding/json"
+	"errors"
 	"fmt"
 	"log"
 	"net/http"
+	"sync"
 
 	"github.com/gorilla/websocket"
 	_ "github.com/jinzhu/gorm/dialects/postgres" // Add the postgres driver
 )
+
+type MessageBase struct {
+	Method string `json:"method"`
+}
+
+type MessageMembers struct {
+	MessageBase
+	Args struct {
+		Members []string `json:"members"`
+	} `json:"args"`
+}
+type MessageSDP struct {
+	MessageBase
+	Args struct {
+		Sdp struct {
+			Type string `json:"sdp"`
+			Sdp  string `json:"type"`
+		} `json:"sdp"`
+		Src string `json:"src"`
+		Dst string `json:"dst"`
+	} `json:"args"`
+}
+type MessageCandidate struct {
+	MessageBase
+	Args struct {
+		Candidate struct {
+			Candidate        string `json:"candidate"`
+			SdpMLineIndex    int    `json:"sdpMLineIndex"`
+			SdpMid           string `json:"sdpMid"`
+			UsernameFragment string `json:"usernameFragment"`
+		} `json:"candidate"`
+		Src string `json:"src"`
+		Dst string `json:"dst"`
+	} `json:"args"`
+}
+
+var membersMap sync.Map
+
+func sendMembers(conn *websocket.Conn) error {
+	message := MessageMembers{MessageBase: MessageBase{Method: "members"}}
+	membersMap.Range(func(key, value interface{}) bool {
+		message.Args.Members = append(message.Args.Members, key.(string))
+		return true
+	})
+	return conn.WriteJSON(message)
+}
+
+func sendSdp(conn *websocket.Conn, raw []byte) error {
+	message := MessageSDP{}
+	if err := json.Unmarshal(raw, &message); err != nil {
+		return err
+	}
+	dstConn, ok := membersMap.Load(message.Args.Dst)
+	if ok == false {
+		return errors.New("No entry found in membersMap")
+	}
+	return dstConn.(*websocket.Conn).WriteJSON(message)
+}
+
+func sendCandidate(conn *websocket.Conn, raw []byte) error {
+	message := MessageCandidate{}
+	if err := json.Unmarshal(raw, &message); err != nil {
+		return err
+	}
+	dstConn, ok := membersMap.Load(message.Args.Dst)
+	if ok == false {
+		return errors.New("No entry found in membersMap")
+	}
+	return dstConn.(*websocket.Conn).WriteJSON(message)
+}
 
 var upgrader = websocket.Upgrader{
 	CheckOrigin: func(r *http.Request) bool {
@@ -15,22 +88,23 @@ var upgrader = websocket.Upgrader{
 	},
 }
 
-func checkAPIKey(apiKey string) (isValid bool) {
-	isValid = true
-	return
-}
-
 // HandleRoot upgrades '/' to websocket
 func HandleRoot(w http.ResponseWriter, r *http.Request) {
-	apiKeys := r.URL.Query()["apiKey"]
-	if len(apiKeys) != 1 {
-		fmt.Println("Bad apiKey count, should be 1", len(apiKeys))
+	checkSessionKey := func(sessionKey string) (isValid bool) {
+		isValid = true
 		return
 	}
-	if !checkAPIKey(apiKeys[0]) {
-		fmt.Println("Invalid apiKey", apiKeys[0])
+
+	sessionKeys := r.URL.Query()["sessionKey"]
+	if len(sessionKeys) != 1 {
+		fmt.Println("Bad sessionKey count, should be 1", len(sessionKeys))
 		return
 	}
+	if !checkSessionKey(sessionKeys[0]) {
+		fmt.Println("Invalid sessionKey", sessionKeys[0])
+		return
+	}
+	sessionKey := sessionKeys[0]
 
 	c, err := upgrader.Upgrade(w, r, nil)
 	if err != nil {
@@ -38,17 +112,50 @@ func HandleRoot(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	defer func() {
+		membersMap.Delete(sessionKey)
 		if err := c.Close(); err != nil {
 			log.Println("Failed to close websocket", err)
 		}
 	}()
 
+	membersMap.Store(sessionKey, c)
+	if err = sendMembers(c); err != nil {
+		log.Println("sendMembers:", err)
+		return
+	}
+
+	message := MessageBase{}
 	for {
-		messageType, message, err := c.ReadMessage()
+		_, raw, err := c.ReadMessage()
 		if err != nil {
 			log.Println("read:", err)
 			return
 		}
-		log.Printf("recv: %s %d", message, messageType)
+
+		if err = json.Unmarshal(raw, &message); err != nil {
+			log.Println("unmarshal:", err)
+			return
+		}
+
+		switch message.Method {
+		case "members":
+			if err = sendMembers(c); err != nil {
+				log.Println("sendMembers:", err)
+				return
+			}
+		case "sdp":
+			if err = sendSdp(c, raw); err != nil {
+				log.Println("sendSdp:", err)
+				return
+			}
+		case "candidate":
+			if err = sendCandidate(c, raw); err != nil {
+				log.Println("sendCandidate:", err)
+				return
+			}
+		default:
+			log.Println("unknown method:", message.Method)
+			return
+		}
 	}
 }
